43f5f58152ee9bdf9869cdbf122f684d8209d448
JulieKristoffersen <Julie.kristoffersen01@yahoo.com>
2025-02-05 11:48:04 +0100

    compressed pictures

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
-function applyInitialTheme() {
-    const userPreference = localStorage.getItem("theme");
-  
-    const systemPreferenceIsDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-  
-    const isDarkMode =
-      userPreference === "dark" || (!userPreference && systemPreferenceIsDark);
-  
-    document.documentElement.classList.toggle("dark", isDarkMode);
-  
-    document.querySelector("#sun").classList.toggle("hidden", isDarkMode);
-    document.querySelector("#moon").classList.toggle("hidden", !isDarkMode);
-  }
-  
-  applyInitialTheme();
-  
-  function toggleTheme() {
-    const isDarkMode = document.documentElement.classList.toggle("dark");
-  
-    localStorage.setItem("theme", isDarkMode ? "dark" : "light");
-  
-    document.querySelector("#sun").classList.toggle("hidden", isDarkMode);
-    document.querySelector("#moon").classList.toggle("hidden", !isDarkMode);
-  }
-  
-  const themeToggle = document.querySelector("#theme-toggle");
-  themeToggle.addEventListener("click", toggleTheme);
+const btn = document.querySelector("#menu-btn");
+const menu = document.querySelector("#mobile-menu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
