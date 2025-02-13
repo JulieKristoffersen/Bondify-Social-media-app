7cab262a382213bf3bbf670bb7083fd4ea89dd46
JulieKristoffersen <Julie.kristoffersen01@yahoo.com>
2025-02-13 13:56:45 +0100

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuBtn = document.querySelector('#menu-btn');
-  const mobileMenu = document.getElementById('mobile-menu');
+const menuBtn = document.getElementById("menu-btn");
+const mobileMenu = document.getElementById("mobile-menu");
 
-  menuBtn.addEventListener('click', function () {
-      const isExpanded = menuBtn.getAttribute('aria-expanded') === 'true';
-      menuBtn.setAttribute('aria-expanded', !isExpanded);
-      mobileMenu.classList.toggle('hidden');
-      mobileMenu.setAttribute('aria-hidden', isExpanded);
-  });
+menuBtn.addEventListener("click", function () {
+    const isExpanded = menuBtn.getAttribute("aria-expanded") === "true";
+    menuBtn.setAttribute("aria-expanded", !isExpanded);
+    mobileMenu.classList.toggle("hidden");
+    mobileMenu.setAttribute("aria-hidden", isExpanded ? "true" : "false");
 });
+
